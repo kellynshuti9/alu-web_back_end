d730c21b17bffa71da170c4ae11238a2b0b317c4
kellynshuti9 <k.dushimima@alustudent.com>
2026-05-21 12:35:12 +0000

    Complete task 6

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -1,9 +1,17 @@
 #!/usr/bin/env python3
-"""Password encryption module"""
+"""Module for password encryption"""
 
 import bcrypt
 
 
 def hash_password(password: str) -> bytes:
-    """Hash a password using bcrypt and return a salted hash"""
-    return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
+    """Hash a password and return a salted hash."""
+    return bcrypt.hashpw(password.encode("utf-8"), bcrypt.gensalt())
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """Validate a password against its hashed version."""
+    return bcrypt.checkpw(
+        password.encode("utf-8"),
+        hashed_password
+    )
