8db1c20d9c9a5756bb87c02a607f6974a087cc08
kellynshuti9 <k.dushimima@alustudent.com>
2026-05-21 08:41:49 +0000

    Task 1 log formatter

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -1,12 +1,31 @@
 #!/usr/bin/env python3
 """Filtered logger module"""
 
+import logging
 import re
-from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str) -> str:
+def filter_datum(fields, redaction, message, separator):
     """Obfuscate sensitive fields in a log message"""
-    pattern = r'(' + '|'.join(fields) + r')=[^' + separator + r']*'
-    return re.sub(pattern, r'\1=' + redaction, message)
+    return re.sub(r'(' + '|'.join(fields) + r')=[^' + separator + r']*',
+                  r'\1=' + redaction, message)
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self, fields):
+        super().__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        return filter_datum(
+            self.fields,
+            self.REDACTION,
+            super().format(record),
+            self.SEPARATOR
+        )
