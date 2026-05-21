e6dc18d7748a4b730c787f7b1ff38297dd566b0e
kellynshuti9 <k.dushimima@alustudent.com>
2026-05-21 08:02:03 +0000

    corrected some mistakes

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -5,7 +5,8 @@ import re
 from typing import List
 
 
-def filter_datum(fields: List[str], redaction: str, message: str, separator: str) -> str:
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
     """Obfuscate sensitive fields in a log message"""
-    pattern = r"(" + "|".join(fields) + r")=[^" + separator + r"]*"
-    return re.sub(pattern, r"\1=" + redaction, message)
+    pattern = r'(' + '|'.join(fields) + r')=[^' + separator + r']*'
+    return re.sub(pattern, r'\1=' + redaction, message)
