4886f42d6169abca899a4d59213cdbe6042a2962
kellynshuti9 <k.dushimima@alustudent.com>
2026-06-25 22:31:13 +0200

    yes

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -2,23 +2,22 @@
 """ 12. Log stats
 """
 
-
 from pymongo import MongoClient
-
 
 def log_stats():
     """ log_stats.
     """
     client = MongoClient('mongodb://127.0.0.1:27017')
-    logs_collection = client.logs.nginx
-    total = logs_collection.count_documents({})
-    get = logs_collection.count_documents({"method": "GET"})
-    post = logs_collection.count_documents({"method": "POST"})
-    put = logs_collection.count_documents({"method": "PUT"})
-    patch = logs_collection.count_documents({"method": "PATCH"})
-    delete = logs_collection.count_documents({"method": "DELETE"})
-    path = logs_collection.count_documents(
-        {"method": "GET", "path": "/status"})
+    collection = client.logs.nginx
+    
+    total = collection.count_documents({})
+    get = collection.count_documents({"method": "GET"})
+    post = collection.count_documents({"method": "POST"})
+    put = collection.count_documents({"method": "PUT"})
+    patch = collection.count_documents({"method": "PATCH"})
+    delete = collection.count_documents({"method": "DELETE"})
+    status_check = collection.count_documents({"method": "GET", "path": "/status"})
+    
     print(f"{total} logs")
     print("Methods:")
     print(f"\tmethod GET: {get}")
@@ -26,8 +25,7 @@ def log_stats():
     print(f"\tmethod PUT: {put}")
     print(f"\tmethod PATCH: {patch}")
     print(f"\tmethod DELETE: {delete}")
-    print(f"{path} status check")
-
+    print(f"{status_check} status check")
 
 if __name__ == "__main__":
     log_stats()
