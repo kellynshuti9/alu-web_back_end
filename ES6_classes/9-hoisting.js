5e69b9050fec799342ce471b261e224dc6904c21
kellynshuti9 <k.dushimima@alustudent.com>
2026-07-03 19:16:27 +0200

    yes

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -1,4 +1,4 @@
-export class HolbertonClass {
+export class ALXClass {
   constructor(year, location) {
     this._year = year;
     this._location = location;
@@ -12,34 +12,35 @@ export class HolbertonClass {
     return this._location;
   }
 }
-const class2019 = new HolbertonClass(2019, 'San Francisco');
-const class2020 = new HolbertonClass(2020, 'San Francisco');
 
-export class StudentHolberton {
-  constructor(firstName, lastName, holbertonClass) {
+const class2019 = new ALXClass(2019, 'San Francisco');
+const class2020 = new ALXClass(2020, 'San Francisco');
+
+export class StudentALX {
+  constructor(firstName, lastName, alxClass) {
     this._firstName = firstName;
     this._lastName = lastName;
-    this._holbertonClass = holbertonClass;
+    this._alxClass = alxClass;
   }
 
   get fullName() {
     return `${this._firstName} ${this._lastName}`;
   }
 
-  get holbertonClass() {
-    return this._holbertonClass;
+  get alxClass() {
+    return this._alxClass;
   }
 
   get fullStudentDescription() {
-    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+    return `${this._firstName} ${this._lastName} - ${this._alxClass.year} - ${this._alxClass.location}`;
   }
 }
 
-const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
-const student2 = new StudentHolberton('John', 'Doe', class2020);
-const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
-const student4 = new StudentHolberton('Donald', 'Bush', class2019);
-const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
+const student1 = new StudentALX('Guillaume', 'Salva', class2020);
+const student2 = new StudentALX('John', 'Doe', class2020);
+const student3 = new StudentALX('Albert', 'Clinton', class2019);
+const student4 = new StudentALX('Donald', 'Bush', class2019);
+const student5 = new StudentALX('Jason', 'Sandler', class2019);
 
 const listOfStudents = [student1, student2, student3, student4, student5];
 export default listOfStudents;
